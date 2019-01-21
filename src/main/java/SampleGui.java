e6aef6f2a79267c3014a0f97b7cabbf13bc23b65
kevin <kknguyen@my.yorku.ca>
2019-01-21 04:38:52 -0500

    Sample JavaFX Gui created

--- src/main/java/SampleGui.java
+++ src/main/java/SampleGui.java
@@ -4,5 +4,19 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class SampleGui {
+public class SampleGui extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Group root = new Group();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
+
