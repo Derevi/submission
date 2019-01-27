d1d389b44a6474a80783af8752aea07d3fb643ed
Auric <kknguyen@my.yorku.ca>
2019-01-26 23:28:53 -0500

    Created text to speech generator that saves audio based on the input string

--- src/main/java/talkbox/gui/StringToAudioGenerator.java
+++ src/main/java/talkbox/gui/StringToAudioGenerator.java
@@ -1,5 +1,25 @@
 package talkbox.gui;
 
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.AudioPlayer;
+import com.sun.speech.freetts.audio.SingleFileAudioPlayer;
+import javax.sound.sampled.AudioFileFormat;
+import java.io.File;
+
+
 public class StringToAudioGenerator {
-    //user input string, creates audio and then it outputs and saves it to a file
+
+    void generateAudio(String word){
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        Voice voice;
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        voice = VoiceManager.getInstance().getVoice("kevin16");
+        voice.allocate();
+        AudioPlayer audioPlayer = new SingleFileAudioPlayer("audio"+ File.separator+word, AudioFileFormat.Type.WAVE);
+        voice.setAudioPlayer(audioPlayer);
+        voice.speak(word);
+        voice.deallocate();
+        audioPlayer.close();
+    }
 }
