dc7e4700efeb267aee01b770fa50ae18b9fb5c3c
Rachel <rachelsmtam@yahoo.com>
2019-02-23 20:53:26 -0500

    Added new button design

--- src/main/java/talkbox/desktop/editor/model/ConfiguratorApp.java
+++ src/main/java/talkbox/desktop/editor/model/ConfiguratorApp.java
@@ -10,16 +10,17 @@ public class ConfiguratorApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-       Parent root = FXMLLoader.load(getClass().getResource("../view/starteditor.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/starteditor.fxml"));
         primaryStage.setTitle("TalkBox Button Editor");
+
+        Scene scene = new Scene(root, 1366, 768);
+        scene.getStylesheets().add(getClass().getResource("../../styles.css").toExternalForm());
 
         primaryStage.setMaxWidth(1366);
         primaryStage.setMinWidth(1366);
         primaryStage.setMaxHeight(768);
         primaryStage.setMinHeight(768);
-        primaryStage.setScene(new Scene(root, 1366, 768));
-
-
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
