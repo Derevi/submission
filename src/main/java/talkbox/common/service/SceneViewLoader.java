cbc58357e227817bbbbdaa410c0d06a4124caf53
Auric <kknguyen@my.yorku.ca>
2019-03-10 17:35:05 -0400

    Fixed SceneViewLoader to load new scene and also set up variable in newly loaded scene

--- src/main/java/talkbox/common/service/SceneViewLoader.java
+++ src/main/java/talkbox/common/service/SceneViewLoader.java
@@ -6,30 +6,47 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import talkbox.desktop.editor.controller.MainEditorController;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SceneViewLoader {
 
-    private Parent parent;
     private String pathToFXML;
+    private File selectedSerFileToLoad;
 
-    public SceneViewLoader(String pathToFXML){
+    public SceneViewLoader(String pathToFXML, File selectedSerFileToLoad){
     this.pathToFXML = pathToFXML;
+    this.selectedSerFileToLoad = selectedSerFileToLoad;
 
     }
 
     public void setNewScene(ActionEvent event){
+        FXMLLoader loader = new FXMLLoader();
+        loaderSetup(loader);
+        initializeControllerVariable(loader.getController(),selectedSerFileToLoad);
+        refreshScene(event, loader.getRoot());
+    }
 
-        try {
-            Parent parent = FXMLLoader.load(getClass().getResource(pathToFXML));
-            Stage newWindow=(Stage)((Node)event.getSource()).getScene().getWindow();
-            newWindow.setScene(new Scene(parent));
-            newWindow.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void loaderSetup(FXMLLoader loader){
+        try{
+            loader.setLocation(getClass().getResource(pathToFXML));
+            loader.load();
+        }catch (IOException ioe){
+            ioe.printStackTrace();
         }
+    }
 
+    private void initializeControllerVariable(MainEditorController mainEditorController, File file){
+        //TODO load and convert catalog and pass through here. Below is just example method to show that variables are being carried over to new FXML
+        mainEditorController.line = file.getName();
+        mainEditorController.setprint(file.getName());
+    }
+
+    private void refreshScene(ActionEvent event, Parent parent){
+        Stage newWindow=(Stage)((Node)event.getSource()).getScene().getWindow();
+        newWindow.setScene(new Scene(parent));
+        newWindow.show();
     }
 }
