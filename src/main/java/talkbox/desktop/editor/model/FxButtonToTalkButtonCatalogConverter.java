c6b0f51ea848067f5263024067e466ff289b13c1
Auric <kknguyen@my.yorku.ca>
2019-03-17 19:32:34 -0400

    Added utility classes gets all data from view and converts to a catalog

---
+++ src/main/java/talkbox/desktop/editor/model/FxButtonToTalkButtonCatalogConverter.java
@@ -0,0 +1,14 @@
+package talkbox.desktop.editor.model;
+
+public class FxButtonToTalkButtonCatalogConverter {
+
+    //TODO, constructor pulls in entire VBOX from view
+    //ignores row 0 since that is the key set
+    //ignores the last button of every row since that is the  "add button" button
+    //ignores any row that contains a separator
+    //creates a new catalog with all the button data, transfer over all member variables (button size, audio file locations etc.
+
+    //TODO ALTERNATIVE: make action event everytime add is called talk button page is altered, this dyanimcally updating it (this may be better)
+
+
+}
